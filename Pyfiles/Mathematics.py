9f8492fd076719cdcf9573158b34fc694e650358
Abhinav Singh <abs.singh34133@Abhinavs-MacBook-Air-2.local>
2024-12-07 02:57:12 +0530

    Initial Commit

--- Pyfiles/Mathematics.py
+++
@@ -1,7 +0,0 @@
-def find_maximum_maxima(input_list):
-    maxima = []
-    for i in range(1, len(input_list) - 1):
-        if input_list[i] > input_list[i - 1] and input_list[i] > input_list[i + 1]:
-            maxima.append(input_list[i])
-    return max(maxima)
-
